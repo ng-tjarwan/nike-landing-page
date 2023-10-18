9c3224d23cc41d022e524a5fa98a39d5d89d09a2
ng-tjarwan <tareq.jarwan@bitoasis.net>
2023-10-18 09:44:31 +0300

    setup the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  Subscribe,
+  Footer,
+  CustomerReviews,
+  SpecialOffer,
+} from "./sections";
+import { Nav } from "./components";
+
 const App = () => {
   return (
     <main className="relative">
-      Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-      <section className="padding">PopularProducts</section>
-      <section className="padding">SuperQuality</section>
-      <section className="padding-x py-10">Services</section>
-      <section className="padding">SpecialOffer</section>
-      <section className="padding bg-pale-blue">CustomerReviews</section>
-      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-      <section className="padding-x padding-t pb-8 bg-black">Footer</section>
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding-x padding-t pb-8 bg-black">
+        <Footer />
+      </section>
     </main>
   );
 };
